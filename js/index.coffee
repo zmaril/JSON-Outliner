bd065abdcb7f3142da4073a64e970686aec9df3c
Zack Maril <zack@zacharymaril.com>
2012-06-11 12:57:49 -0200

    Initing

---
+++ js/index.coffee
@@ -0,0 +1,63 @@
+graphic = new Object
+radius = 10
+size = 0
+delta = 10
+route = null
+
+graphic.create = ()->
+  width = $(document).width()/2
+  height = $(document).height()*.85
+  size = d3.min([width,height])
+
+  graphic.svg = d3.select("#graphic")
+    .append("svg")
+      .attr("width",size)
+      .attr("height",size)
+
+  graphic.g = graphic.svg.append("g")
+    .attr("transform","translate(#{size/2},#{size/2})")
+
+  graphic.circ = graphic.g.append("circle")
+    .attr("fill","black")
+    .attr("r",radius)
+
+  graphic.slider = $("#slider").slider(
+      value : radius
+      min : 1
+      max : size
+      step: 0.01
+      slide : (event,ui)-> route.navigate("//#{ui.value}",{trigger: true}))
+
+
+graphic.update = ()->
+  graphic.circ.transition(100).attr("r",radius)
+  graphic.slider.slider("value",radius)
+
+graphic.destroy = ()->
+  graphic.svg.remove()
+  delete graphic.svg
+
+HashBangs = Backbone.Router.extend({
+    "routes":
+      "": "default"
+      ":radius" : "radius"
+
+    "default": ()->
+
+    "radius": (r)->
+
+      radius = r
+      graphic.update()
+  })
+
+start_backbone = ()->
+  route = new HashBangs()
+  Backbone.history.start()
+
+#  Backbone.history.start()
+$(document).ready ()->
+  graphic.create()
+  start_backbone()
+  $(window).resize ()->
+    graphic.destroy()
+    graphic.create()
---
+++ js/index.js
@@ -0,0 +1,68 @@
+var HashBangs, delta, graphic, radius, route, size, start_backbone;
+
+graphic = new Object;
+
+radius = 10;
+
+size = 0;
+
+delta = 10;
+
+route = null;
+
+graphic.create = function() {
+  var height, width;
+  width = $(document).width() / 2;
+  height = $(document).height() * .85;
+  size = d3.min([width, height]);
+  graphic.svg = d3.select("#graphic").append("svg").attr("width", size).attr("height", size);
+  graphic.g = graphic.svg.append("g").attr("transform", "translate(" + (size / 2) + "," + (size / 2) + ")");
+  graphic.circ = graphic.g.append("circle").attr("fill", "black").attr("r", radius);
+  return graphic.slider = $("#slider").slider({
+    value: radius,
+    min: 1,
+    max: size,
+    step: 0.01,
+    slide: function(event, ui) {
+      return route.navigate("//" + ui.value, {
+        trigger: true
+      });
+    }
+  });
+};
+
+graphic.update = function() {
+  graphic.circ.transition(100).attr("r", radius);
+  return graphic.slider.slider("value", radius);
+};
+
+graphic.destroy = function() {
+  graphic.svg.remove();
+  return delete graphic.svg;
+};
+
+HashBangs = Backbone.Router.extend({
+  "routes": {
+    "": "default",
+    ":radius": "radius"
+  },
+  "default": function() {},
+  "radius": function(r) {
+    radius = r;
+    return graphic.update();
+  }
+});
+
+start_backbone = function() {
+  route = new HashBangs();
+  return Backbone.history.start();
+};
+
+$(document).ready(function() {
+  graphic.create();
+  start_backbone();
+  return $(window).resize(function() {
+    graphic.destroy();
+    return graphic.create();
+  });
+});
