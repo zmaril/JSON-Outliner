86c0689ec398890feb7bb676cea2dde8307618ee
Zack Maril <zack@zacharymaril.com>
2012-06-11 23:55:30 -0200

    Got it up and working to a certain point. Still need to add in the ordering for the points and interaction elements.

--- js/index.coffee
+++ js/index.coffee
@@ -1,63 +1,202 @@
-graphic = new Object
-radius = 10
-size = 0
-delta = 10
-route = null
+#TODOs:
+# Add in a json outputter
+# Make it easier to take out items
+# Fisheye for moving apart nodes
+# Expand on hover
+# Better pixel finder
 
-graphic.create = ()->
-  width = $(document).width()/2
-  height = $(document).height()*.85
-  size = d3.min([width,height])
+# Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = (from, to) ->
+  rest = @slice((to or from) + 1 or @length)
+  @length = if from < 0 then @length + from else from
+  @push.apply(this, rest);
 
-  graphic.svg = d3.select("#graphic")
-    .append("svg")
-      .attr("width",size)
-      .attr("height",size)
 
-  graphic.g = graphic.svg.append("g")
-    .attr("transform","translate(#{size/2},#{size/2})")
+unchanged = {}
+bw = {}
+points = {
+  fisheye : d3.fisheye().radius(50).power(2)
+  threshold: 195
+  edges: []
+  }
 
-  graphic.circ = graphic.g.append("circle")
+convert_to_bw = ()->
+  bw.imageData = unchanged.context.getImageData(0,0,unchanged.width,unchanged.height)
+  pixels = bw.imageData.data
+  for i in [0..(unchanged.height*unchanged.width)]
+    r = pixels[i*4]
+    g = pixels[i*4+1]
+    b = pixels[i*4+2]
+
+    #http://en.wikipedia.org/wiki/Grayscale
+    grey = Math.round(0.3*r+0.59*g+0.11*b)
+
+    pixels[i*4] = grey
+    pixels[i*4+1] = grey
+    pixels[i*4+2] = grey
+
+
+display_bw = ()->
+  bw.canvas = document.getElementById("blackwhite_c")
+  bw.context = bw.canvas.getContext("2d")
+  bw.canvas.setAttribute('height',bw.imageData.height)
+  bw.canvas.setAttribute('width',bw.imageData.width)
+  bw.context.putImageData(bw.imageData,0,0)
+
+find_points = ()->
+  console.log(points.threshold,points.edges.length)
+  points.imageData = bw.imageData
+  pixels = points.imageData.data
+
+  tmp = []
+
+  for i in [(unchanged.width*1)..(unchanged.height*(unchanged.width-1))]
+    left = pixels[(i-1)*4]
+    right = pixels[(i+1)*4]
+    top = pixels[(i-unchanged.width)*4]
+    bottom = pixels[(i+unchanged.width)*4]
+
+    horizon = (right-left)/2
+    vertical = (bottom-top)/2
+
+    #http://en.wikipedia.org/wiki/Edge_detection
+    #Implement canny edge detection for better edges.
+    edge = 255-Math.sqrt(horizon*horizon+vertical*vertical)
+
+    tmp[i*4] = edge
+    tmp[i*4+1] = edge
+    tmp[i*4+2] = edge
+
+
+  points.edges = []
+
+  for i in [1..(unchanged.height*unchanged.width)]
+      e = tmp[i*4]
+      if e < points.threshold
+        e = 0
+        x = i%unchanged.width
+        y = (i-x)/unchanged.width
+        points.edges.push(x: x, y: y)
+
+display_points = ()->
+  points.viz
+    .attr("height",unchanged.height)
+    .attr("width",unchanged.width)
+
+  points.circs = points.g.selectAll("circle").data(points.edges)
+
+  points.circs.enter().append("circle")
+
+  points.circs
+    .attr("r",1)
     .attr("fill","black")
-    .attr("r",radius)
+    .attr("transform",(d)-> "translate(#{d.x},#{d.y})")
 
-  graphic.slider = $("#slider").slider(
-      value : radius
-      min : 1
-      max : size
-      step: 0.01
-      slide : (event,ui)-> route.navigate("//#{ui.value}",{trigger: true}))
+  points.circs.exit().remove()
+
+process_new_image = ()->
+  convert_to_bw()
+  display_bw()
+  find_points()
+  display_points()
+
+load_new_image = (source)->
+  $org = $("img#original").attr("src",source)
+
+  unchanged.image = new Image()
+  unchanged.image.src = source
+
+  done = ()->
+    counter = 0
+    ()->
+      counter++
+      counter is 2
+
+  go = ()->
+    unchanged.height = $org.height()
+    unchanged.width = $org.width()
+    $canvas = $("canvas#original")
+      .attr({"width": unchanged.width, "height": unchanged.height})
+    unchanged.canvas = $canvas[0]
+    unchanged.context = unchanged.canvas.getContext("2d")
+    unchanged.context.drawImage(unchanged.image,0,0,unchanged.width,unchanged.height)
+    process_new_image()
+
+  gone = false
+
+  f = ()-> if done() and not gone then go() and gone = true
+
+  $org.load f
+  $(unchanged.image).load f
+
+fish_points = (e)->
+  x = e.offsetX
+  y = e.offsetY
+  m = $("#points > svg").offset()
+  if !x?
+    totalOffsetX = 0
+    totalOffsetY = 0
+    currentElement = this
+    while true
+      totalOffsetX += currentElement.offsetLeft
+      totalOffsetY += currentElement.offsetTop
+      break if (currentElement = currentElement.offsetParent)
+
+    x = e.pageX - totalOffsetX
+    y = e.pageY - totalOffsetY
+
+  points.fisheye.center([x,y])
+  points.circs
+   .each((d)->
+      move = points.fisheye(d)
+      $(this).tooltip(
+        title: "[#{d.x},#{d.y}]"
+        placement: "top"
+      )
+      point = d3.select(this)
+      if move.x is d.x and move.y is d.y
+          point
+          .attr("r",1)
+          .attr("transform",(d)-> "translate(#{d.x},#{d.y})")
+      else
+        point
+          .attr("transform",(d)-> "translate(#{move.x},#{move.y})")
+          .attr("r",3)
+  )
+
+setup = ()->
+  $(".nav-tabs a:last").tab('show')
+
+  handleFileUpload = (e)->
+    file = e.target.files[0]
+    if !file.type.match('image.*') then return
+
+    reader = new FileReader()
+
+    reader.onload = (e)->
+      load_new_image(e.target.result)
+
+    reader.readAsDataURL(file)
+
+  file_input = $("#file").on('change',handleFileUpload)
+
+  load_new_image("./img/leaf.jpg")
+
+  points.viz = d3.select("div#points").append("svg")
+  points.g = points.viz.append("g")
 
 
-graphic.update = ()->
-  graphic.circ.transition(100).attr("r",radius)
-  graphic.slider.slider("value",radius)
+  $("#points").on(i,fish_points) for i in ["mousemove","mousein","mouseout"]
 
-graphic.destroy = ()->
-  graphic.svg.remove()
-  delete graphic.svg
+  $("#threshold-slider").slider(
+    max: 255
+    min: 0
+    step: 1
+    value: points.threshold
+    slide: (event,ui)->
+      points.threshold = ui.value
+      find_points()
+      display_points()
+  )
 
-HashBangs = Backbone.Router.extend({
-    "routes":
-      "": "default"
-      ":radius" : "radius"
-
-    "default": ()->
-
-    "radius": (r)->
-
-      radius = r
-      graphic.update()
-  })
-
-start_backbone = ()->
-  route = new HashBangs()
-  Backbone.history.start()
-
-#  Backbone.history.start()
-$(document).ready ()->
-  graphic.create()
-  start_backbone()
-  $(window).resize ()->
-    graphic.destroy()
-    graphic.create()
+setup()
--- js/index.js
+++ js/index.js
@@ -1,68 +1,206 @@
-var HashBangs, delta, graphic, radius, route, size, start_backbone;
+var bw, convert_to_bw, display_bw, display_points, find_points, fish_points, load_new_image, points, process_new_image, setup, unchanged;
 
-graphic = new Object;
+Array.prototype.remove = function(from, to) {
+  var rest;
+  rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
 
-radius = 10;
+unchanged = {};
 
-size = 0;
+bw = {};
 
-delta = 10;
+points = {
+  fisheye: d3.fisheye().radius(50).power(2),
+  threshold: 195,
+  edges: []
+};
 
-route = null;
+convert_to_bw = function() {
+  var b, g, grey, i, pixels, r, _ref, _results;
+  bw.imageData = unchanged.context.getImageData(0, 0, unchanged.width, unchanged.height);
+  pixels = bw.imageData.data;
+  _results = [];
+  for (i = 0, _ref = unchanged.height * unchanged.width; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+    r = pixels[i * 4];
+    g = pixels[i * 4 + 1];
+    b = pixels[i * 4 + 2];
+    grey = Math.round(0.3 * r + 0.59 * g + 0.11 * b);
+    pixels[i * 4] = grey;
+    pixels[i * 4 + 1] = grey;
+    _results.push(pixels[i * 4 + 2] = grey);
+  }
+  return _results;
+};
 
-graphic.create = function() {
-  var height, width;
-  width = $(document).width() / 2;
-  height = $(document).height() * .85;
-  size = d3.min([width, height]);
-  graphic.svg = d3.select("#graphic").append("svg").attr("width", size).attr("height", size);
-  graphic.g = graphic.svg.append("g").attr("transform", "translate(" + (size / 2) + "," + (size / 2) + ")");
-  graphic.circ = graphic.g.append("circle").attr("fill", "black").attr("r", radius);
-  return graphic.slider = $("#slider").slider({
-    value: radius,
-    min: 1,
-    max: size,
-    step: 0.01,
-    slide: function(event, ui) {
-      return route.navigate("//" + ui.value, {
-        trigger: true
+display_bw = function() {
+  bw.canvas = document.getElementById("blackwhite_c");
+  bw.context = bw.canvas.getContext("2d");
+  bw.canvas.setAttribute('height', bw.imageData.height);
+  bw.canvas.setAttribute('width', bw.imageData.width);
+  return bw.context.putImageData(bw.imageData, 0, 0);
+};
+
+find_points = function() {
+  var bottom, e, edge, horizon, i, left, pixels, right, tmp, top, vertical, x, y, _ref, _ref2, _ref3, _results;
+  console.log(points.threshold, points.edges.length);
+  points.imageData = bw.imageData;
+  pixels = points.imageData.data;
+  tmp = [];
+  for (i = _ref = unchanged.width * 1, _ref2 = unchanged.height * (unchanged.width - 1); _ref <= _ref2 ? i <= _ref2 : i >= _ref2; _ref <= _ref2 ? i++ : i--) {
+    left = pixels[(i - 1) * 4];
+    right = pixels[(i + 1) * 4];
+    top = pixels[(i - unchanged.width) * 4];
+    bottom = pixels[(i + unchanged.width) * 4];
+    horizon = (right - left) / 2;
+    vertical = (bottom - top) / 2;
+    edge = 255 - Math.sqrt(horizon * horizon + vertical * vertical);
+    tmp[i * 4] = edge;
+    tmp[i * 4 + 1] = edge;
+    tmp[i * 4 + 2] = edge;
+  }
+  points.edges = [];
+  _results = [];
+  for (i = 1, _ref3 = unchanged.height * unchanged.width; 1 <= _ref3 ? i <= _ref3 : i >= _ref3; 1 <= _ref3 ? i++ : i--) {
+    e = tmp[i * 4];
+    if (e < points.threshold) {
+      e = 0;
+      x = i % unchanged.width;
+      y = (i - x) / unchanged.width;
+      _results.push(points.edges.push({
+        x: x,
+        y: y
+      }));
+    } else {
+      _results.push(void 0);
+    }
+  }
+  return _results;
+};
+
+display_points = function() {
+  points.viz.attr("height", unchanged.height).attr("width", unchanged.width);
+  points.circs = points.g.selectAll("circle").data(points.edges);
+  points.circs.enter().append("circle");
+  points.circs.attr("r", 1).attr("fill", "black").attr("transform", function(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  });
+  return points.circs.exit().remove();
+};
+
+process_new_image = function() {
+  convert_to_bw();
+  display_bw();
+  find_points();
+  return display_points();
+};
+
+load_new_image = function(source) {
+  var $org, done, f, go, gone;
+  $org = $("img#original").attr("src", source);
+  unchanged.image = new Image();
+  unchanged.image.src = source;
+  done = function() {
+    var counter;
+    counter = 0;
+    return function() {
+      counter++;
+      return counter === 2;
+    };
+  };
+  go = function() {
+    var $canvas;
+    unchanged.height = $org.height();
+    unchanged.width = $org.width();
+    $canvas = $("canvas#original").attr({
+      "width": unchanged.width,
+      "height": unchanged.height
+    });
+    unchanged.canvas = $canvas[0];
+    unchanged.context = unchanged.canvas.getContext("2d");
+    unchanged.context.drawImage(unchanged.image, 0, 0, unchanged.width, unchanged.height);
+    return process_new_image();
+  };
+  gone = false;
+  f = function() {
+    if (done() && !gone) return go() && (gone = true);
+  };
+  $org.load(f);
+  return $(unchanged.image).load(f);
+};
+
+fish_points = function(e) {
+  var currentElement, m, totalOffsetX, totalOffsetY, x, y;
+  x = e.offsetX;
+  y = e.offsetY;
+  m = $("#points > svg").offset();
+  if (!(x != null)) {
+    totalOffsetX = 0;
+    totalOffsetY = 0;
+    currentElement = this;
+    while (true) {
+      totalOffsetX += currentElement.offsetLeft;
+      totalOffsetY += currentElement.offsetTop;
+      if ((currentElement = currentElement.offsetParent)) break;
+    }
+    x = e.pageX - totalOffsetX;
+    y = e.pageY - totalOffsetY;
+  }
+  points.fisheye.center([x, y]);
+  return points.circs.each(function(d) {
+    var move, point;
+    move = points.fisheye(d);
+    $(this).tooltip({
+      title: "[" + d.x + "," + d.y + "]",
+      placement: "top"
+    });
+    point = d3.select(this);
+    if (move.x === d.x && move.y === d.y) {
+      return point.attr("r", 1).attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
       });
+    } else {
+      return point.attr("transform", function(d) {
+        return "translate(" + move.x + "," + move.y + ")";
+      }).attr("r", 3);
     }
   });
 };
 
-graphic.update = function() {
-  graphic.circ.transition(100).attr("r", radius);
-  return graphic.slider.slider("value", radius);
-};
-
-graphic.destroy = function() {
-  graphic.svg.remove();
-  return delete graphic.svg;
-};
-
-HashBangs = Backbone.Router.extend({
-  "routes": {
-    "": "default",
-    ":radius": "radius"
-  },
-  "default": function() {},
-  "radius": function(r) {
-    radius = r;
-    return graphic.update();
+setup = function() {
+  var file_input, handleFileUpload, i, _i, _len, _ref;
+  $(".nav-tabs a:last").tab('show');
+  handleFileUpload = function(e) {
+    var file, reader;
+    file = e.target.files[0];
+    if (!file.type.match('image.*')) return;
+    reader = new FileReader();
+    reader.onload = function(e) {
+      return load_new_image(e.target.result);
+    };
+    return reader.readAsDataURL(file);
+  };
+  file_input = $("#file").on('change', handleFileUpload);
+  load_new_image("./img/leaf.jpg");
+  points.viz = d3.select("div#points").append("svg");
+  points.g = points.viz.append("g");
+  _ref = ["mousemove", "mousein", "mouseout"];
+  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+    i = _ref[_i];
+    $("#points").on(i, fish_points);
   }
-});
-
-start_backbone = function() {
-  route = new HashBangs();
-  return Backbone.history.start();
+  return $("#threshold-slider").slider({
+    max: 255,
+    min: 0,
+    step: 1,
+    value: points.threshold,
+    slide: function(event, ui) {
+      points.threshold = ui.value;
+      find_points();
+      return display_points();
+    }
+  });
 };
 
-$(document).ready(function() {
-  graphic.create();
-  start_backbone();
-  return $(window).resize(function() {
-    graphic.destroy();
-    return graphic.create();
-  });
-});
+setup();
